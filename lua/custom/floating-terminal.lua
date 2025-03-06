fe22e064b9f11d03a59ed4047f00bcd0522d3d26
Joshua Molloy <joshuajmolloy@gmail.com>
2025-03-06 22:19:11 +0000

    feat: added ipython repl floating window

--- lua/custom/floating-terminal.lua
+++ lua/custom/floating-terminal.lua
@@ -1,14 +1,19 @@
 local M = {}
 
 local state = {
-  floating = {
+  terminal = {
+    buf = -1,
+    win = -1,
+    job_id = -1,
+  },
+  ipython = {
     buf = -1,
     win = -1,
     job_id = -1,
   },
 }
 
-local function create_floating_term(opts)
+local function create_floating_window(opts)
   opts = opts or {}
 
   local width = math.floor(vim.o.columns * 0.8)
@@ -48,27 +53,45 @@ local function file_exists(filepath)
 end
 
 M.toggle_terminal = function()
-  if state.floating.win == -1 or not vim.api.nvim_win_is_valid(state.floating.win) then
-    state.floating = create_floating_term { buf = state.floating.buf }
-    if vim.bo[state.floating.buf].buftype ~= 'terminal' then
-      state.floating.job_id = vim.fn.termopen(vim.o.shell)
+  if state.terminal.win == -1 or not vim.api.nvim_win_is_valid(state.terminal.win) then
+    state.terminal = create_floating_window { buf = state.terminal.buf }
+    if vim.bo[state.terminal.buf].buftype ~= 'terminal' then
+      state.terminal.job_id = vim.fn.termopen(vim.o.shell)
 
       if file_exists '.envrc' then
         vim.defer_fn(function()
-          if state.floating.job_id then
-            vim.fn.chansend(state.floating.job_id, 'direnv reload\n')
+          if state.terminal.job_id then
+            vim.fn.chansend(state.terminal.job_id, 'direnv reload\n')
           end
         end, 100)
       end
     end
   else
-    vim.api.nvim_win_hide(state.floating.win)
-    state.floating.win = -1
+    vim.api.nvim_win_hide(state.terminal.win)
+    state.terminal.win = -1
+  end
+end
+
+M.toggle_ipython_repl = function()
+  if state.ipython.win == -1 or not vim.api.nvim_win_is_valid(state.ipython.win) then
+    state.ipython = create_floating_window { buf = state.ipython.buf }
+    if vim.bo[state.ipython.buf].buftype ~= 'terminal' then
+      state.ipython.job_id = vim.fn.termopen('ipython --no-autoindent', {
+        on_exit = function()
+          state.ipython.win = -1
+        end,
+      })
+    end
+  else
+    vim.api.nvim_win_hide(state.ipython.win)
+    state.ipython.win = -1
   end
 end
 
 -- Custom command for opening and closing floating terminal
 vim.api.nvim_create_user_command('FloatingTerminal', M.toggle_terminal, {})
 vim.keymap.set({ 'n' }, '<leader>tt', M.toggle_terminal)
+vim.api.nvim_create_user_command('FloatingIPythonIde', M.toggle_ipython_repl, {})
+vim.keymap.set({ 'n' }, '<leader>tp', M.toggle_ipython_repl)
 
 return M
