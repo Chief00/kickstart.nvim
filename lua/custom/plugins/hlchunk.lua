65636a1d4b709445970057f01219abb5bd6c30d7
Joshua Molloy <josh.molloy@ddcas.ai>
2026-02-03 19:17:20 +0000

    feat: Add hlchunk - indent line and context indicators

---
+++ lua/custom/plugins/hlchunk.lua
@@ -0,0 +1,12 @@
+-- This is for showing the code block with arrows and highlights
+return {
+  'shellRaining/hlchunk.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    require('hlchunk').setup {
+      chunk = {
+        enable = true,
+      },
+    }
+  end,
+}
