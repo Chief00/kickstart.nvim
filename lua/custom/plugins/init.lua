751a46a143f442bf0cbece182f222758a784d3e3
Joshua Molloy <josh@compendium.solutions>
2026-03-13 16:48:58 +0000

    feat: Updates for treesitter; Add more colourschemes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,4 +12,27 @@ return {
     end,
   },
   { 'nvim-neotest/nvim-nio' },
+  { 'rebelot/kanagawa.nvim' },
+  {
+    'vague-theme/vague.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require('vague').setup {
+        -- optional configuration here
+      }
+      vim.cmd 'colorscheme vague'
+    end,
+  },
+  {
+    'uhs-robert/oasis.nvim',
+    lazy = false,
+    priority = 999,
+    config = function()
+      require('oasis').setup() -- (see Configuration below for all customization options)
+      vim.cmd.colorscheme 'oasis-abyss' -- After setup, apply theme (or any style like "oasis-night")
+    end,
+  },
+  { 'EdenEast/nightfox.nvim' }, -- lazy
 }
